4a2d2426a68bd5c441f7f431aa28ab9e01ceeb06
hpramanik <himangshu_pramanik@crgl-thirdparty.com>
2020-03-05 15:20:46 +0530

    Default profile

--- sf-di/src/main/java/io/hspx/sfdi/services/I18nEnglishGreetingService.java
+++ sf-di/src/main/java/io/hspx/sfdi/services/I18nEnglishGreetingService.java
@@ -3,7 +3,8 @@ package io.hspx.sfdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({ "EN", "default" }) // Adding it as default. Now if the active profile is not set in
+                              // application.properties it will wire up the I18EnglishGreetingService
 @Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService {
 
