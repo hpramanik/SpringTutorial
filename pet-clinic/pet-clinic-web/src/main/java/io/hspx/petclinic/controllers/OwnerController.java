77b2c6d3eb68935e6dc83439dfbc0c3c4e21727f
hpramanik <himangshu_pramanik@crgl-thirdparty.com>
2020-03-06 03:08:29 +0530

    Refactored request mapping

--- pet-clinic/pet-clinic-web/src/main/java/io/hspx/petclinic/controllers/OwnerController.java
+++ pet-clinic/pet-clinic-web/src/main/java/io/hspx/petclinic/controllers/OwnerController.java
@@ -3,9 +3,10 @@ package io.hspx.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"/", "/index", "/index.html"})
     public String listVets() {
         return "owners/index";
     }
