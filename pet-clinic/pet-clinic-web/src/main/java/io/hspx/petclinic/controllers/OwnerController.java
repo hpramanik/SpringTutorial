5ba8394e2fe23988f3502eb26579dda88f07eb19
hpramanik <himangshu_pramanik@crgl-thirdparty.com>
2020-03-06 13:22:55 +0530

    View - Displaying owners list. Closes #16

--- pet-clinic/pet-clinic-web/src/main/java/io/hspx/petclinic/controllers/OwnerController.java
+++ pet-clinic/pet-clinic-web/src/main/java/io/hspx/petclinic/controllers/OwnerController.java
@@ -1,13 +1,22 @@
 package io.hspx.petclinic.controllers;
 
+import io.hspx.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
