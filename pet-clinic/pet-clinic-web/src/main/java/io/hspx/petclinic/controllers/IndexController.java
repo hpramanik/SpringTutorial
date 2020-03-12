c15148a00038abcb433e9fd69d84efc471668ae6
hpramanik <himangshu_pramanik@crgl-thirdparty.com>
2020-03-12 09:30:53 +0530

    Fixed broken links. Closes #45

--- pet-clinic/pet-clinic-web/src/main/java/io/hspx/petclinic/controllers/IndexController.java
+++ pet-clinic/pet-clinic-web/src/main/java/io/hspx/petclinic/controllers/IndexController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
     @RequestMapping({"", "/", "index", "index.html"})
-    public String index(){
+    public String index() {
         return "index";
+    }
+
+    @RequestMapping({"/oops", "/error", "/oups"})
+    public String error() {
+        return "notImplemented";
     }
 }
