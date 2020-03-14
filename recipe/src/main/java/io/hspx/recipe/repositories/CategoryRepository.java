ca5cf392ba355c3934927c1965b53043e2d6d826
hpramanik <himangshu_pramanik@crgl-thirdparty.com>
2020-03-14 19:44:41 +0530

    Created Repositories For recipe application.

--- recipe/src/main/java/io/hspx/recipe/repositories/CategoryRepository.java
+++ recipe/src/main/java/io/hspx/recipe/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package io.hspx.recipe.repositories;
 import io.hspx.recipe.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
--- recipe/src/main/java/io/hspx/recipe/repositories/UnitOfMeasureRepository.java
+++ recipe/src/main/java/io/hspx/recipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package io.hspx.recipe.repositories;
 import io.hspx.recipe.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByUom(String uom);
 }
