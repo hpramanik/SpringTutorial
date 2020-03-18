597cbf13d4b715dd7fff13a58bb8688acbed00ee
hpramanik <himangshu_pramanik@crgl-thirdparty.com>
2020-03-18 07:03:02 +0530

    Implemented Equals method for BaseEntity. Closes #12

--- pet-clinic/pet-clinic-data/src/main/java/io/hspx/petclinic/models/BaseEntity.java
+++ pet-clinic/pet-clinic-data/src/main/java/io/hspx/petclinic/models/BaseEntity.java
@@ -5,6 +5,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
+import java.util.Objects;
 
 @MappedSuperclass
 public class BaseEntity implements Serializable {
@@ -18,5 +19,18 @@ public class BaseEntity implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
